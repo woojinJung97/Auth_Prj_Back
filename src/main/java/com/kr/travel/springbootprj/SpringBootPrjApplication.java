8ae6c65de13a71ce44c8c25513983a8a9822df44
woojinJung97 <wjddnwls9709@gmail.com>
2024-10-09 14:55:07 +0900

    first upload

---
+++ src/main/java/com/kr/travel/springbootprj/SpringBootPrjApplication.java
@@ -0,0 +1,13 @@
+package com.kr.travel.springbootprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kr/travel/springbootprj/SpringBootPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.travel.springbootprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
